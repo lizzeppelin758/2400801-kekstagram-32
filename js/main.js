05bea506b07e772678e954e0750f023f710a9e5a
lizzeppelin758 <lizzeppelin758@gmail.com>
2024-07-19 10:16:21 +0600

    Добавляет появление комментариев

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import {createPosts} from './data.js';
 import {generateThumbnails} from './thumbnail.js';
 import './picture-modal.js';
 
+
 const currentPictures = createPosts();
 
 generateThumbnails(currentPictures);
 
 export {currentPictures};
+
